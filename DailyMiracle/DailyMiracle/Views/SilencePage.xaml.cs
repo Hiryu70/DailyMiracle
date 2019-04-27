37181314a3f1722459952a59513bde2eb8b54c18
Hiryu70 <che.maxv@gmail.com>
2019-04-27 18:31:04 +0300

    Add project files.

---
+++ DailyMiracle/DailyMiracle/Views/SilencePage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using DailyMiracle.Models;
+using DailyMiracle.ViewModels;
+
+namespace DailyMiracle.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class SilencePage : ContentPage
+    {
+        ItemDetailViewModel viewModel;
+
+        public SilencePage(ItemDetailViewModel viewModel)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+        }
+
+        public SilencePage()
+        {
+            InitializeComponent();
+
+            var item = new Item
+            {
+                Text = "Item 1",
+                Description = "This is an item description."
+            };
+
+            viewModel = new ItemDetailViewModel(item);
+            BindingContext = viewModel;
+        }
+    }
+}
